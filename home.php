47f0fb566d907f1fe9b769e04c59cf4dfad914a9
Harli Kodasma <harlikodasma@users.noreply.github.com>
2020-09-14 19:53:09 +0300

    kodune töö, 2 veaga

--- home.php
+++ home.php
@@ -6,8 +6,17 @@
 	if($hournow < 6) {
 		$partofday = "uneaeg";
 	}
+	if($hournow > 5 and $hournow < 8) {
+		$partofday = "kooli minemise aeg";
+	}
 	if($hournow >= 8 and $hournow <= 18) {
 		$partofday = "õppimise aeg";
+	}
+	if($hournow > 18 and $hournow < 20) {
+		$partofday = "söömise aeg";
+	}
+	if($hournow > 19 and $hournow < 24) {
+		$partofday = "teleka aeg";
 	}
 	
 	//vaatame semestri kulgemist
@@ -16,6 +25,45 @@
 	$semesterduration = $semesterstart->diff($semesterend);
 	$semesterdurationdays = $semesterduration->format("%r%a");
 	$today = new DateTime("now");
+	$fromsemesterstartuntiltoday = $semesterstart->diff($today);
+	$daysfromsemesterstart = $fromsemesterstartuntiltoday->format("%r%a");
+	$daysuntilend = $semesterdurationdays - $daysfromsemesterstart;
+	$percentagecalculation = round(($daysfromsemesterstart * 100) / $semesterdurationdays, 2);
+	if($daysfromsemesterstart < 0) {
+		$daysfromsemesterstartformatted = -$daysfromsemesterstart;
+		$semesterpercentage = "Semestri õppetööst on tehtud 0%.";
+		if($daysfromsemesterstartformatted == 1) {
+			$semesterprintout = "Semester ei ole veel alanud. Alguseni on jäänud " .$daysfromsemesterstartformatted ." päev.";
+		} else {
+		$semesterprintout = "Semester ei ole veel alanud. Alguseni on jäänud " .$daysfromsemesterstartformatted ." päeva.";
+		}
+	} elseif($daysfromsemesterstart == 0) {
+		$semesterpercentage = "Semestri õppetööst on tehtud 0%.";
+		$semesterprintout = "Täna algab semester.";
+	} elseif($daysfromsemesterstart > 0 and $daysfromsemesterstart <= $semesterdurationdays) {
+		$isitone = $semesterdurationdays - $daysfromsemesterstart;
+		$semesterpercentage = "Semestri õppetööst on tehtud " .$percentagecalculation ."%";
+		if($isitone == 1) {
+			$semesterprintout = "Semester käib. See on käinud juba " .$daysfromsemesterstart ." päeva." ." Lõpuni on jäänud " .$daysuntilend ." päev.";
+		} elseif($daysfromsemesterstart == 1) {
+			$semesterprintout = "Semester käib. See on käinud juba " .$daysfromsemesterstart ." päev." ." Lõpuni on jäänud " .$daysuntilend ." päeva.";
+		} elseif($isitone > 1 and $daysfromsemesterstart > 1) {
+			$semesterprintout = "Semester käib. See on käinud juba " .$daysfromsemesterstart ." päeva." ." Lõpuni on jäänud " .$daysuntilend ." päeva.";
+		}
+	} elseif($daysfromsemesterstart == $semesterdurationdays) {
+		$semesterprintout = "Täna on semestri viimane päev. Semester on käinud " .$semesterdurationdays ." päeva.";
+		$semesterpercentage = "Semestri õppetööst on tehtud " .$percentagecalculation ."%";
+	} elseif($daysfromsemesterstart > $semesterdurationdays) {
+		$daysfromsemesterend = $daysfromsemesterstart - $semesterdurationdays;
+		$semesterpercentage = "Semestri õppetööst on tehtud 100%.";
+		if($daysfromsemesterend == 1) {
+			$semesterprintout = "Semester on läbi. Semestri lõpust on möödas " .$daysfromsemesterend ." päev.";
+		} else {
+		$semesterprintout = "Semester on läbi. Semestri lõpust on möödas " .$daysfromsemesterend ." päeva.";
+		}
+	}
+	//2020-8-31
+	//2020-12-13
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -31,5 +79,9 @@
   <p>Kodutööna panin ühe lause juurde.</p>
   <p>Lehe avamise hetk: <?php echo $fulltimenow; ?>.</p>
   <p><?php echo "Praegu on " .$partofday ."."; ?></p>
+  
+  <p>Semester kestab kokku <?php echo $semesterdurationdays; ?> päeva.</p>
+  <p><?php echo $semesterprintout; ?></p>
+  <p><?php echo $semesterpercentage; ?></p>
 </body>
 </html>
