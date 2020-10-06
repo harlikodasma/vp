2b679a8cf02ceab87f0c6fd476c4683a3fad673b
Harli Kodasma <harlikodasma@users.noreply.github.com>
2020-10-06 13:48:13 +0300

    kuues tund lõppenud

---
+++ tund6/listfilms.php
@@ -0,0 +1,21 @@
+<?php
+	require("usesession.php");
+	require("../../../config.php");
+	require("fnc_films.php");
+	//$filmhtml = readfilms();
+	require("header.php");
+?>
+  <img src="../img/vp_banner_improved.png" alt="Veebiprogrammeerimise kursuse bänner">
+  <h1><?php echo $_SESSION["userfirstname"] ." " .$_SESSION["userlastname"]; ?></h1>
+  <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p>See leht on tehtud veebiprogrammeerimise kursusel 2020. aasta sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
+    <p><a href="?logout=1">Logi välja</a></p>
+  <hr>
+  
+  <p><a href="home.php">Tagasi avalehele</a></p>
+  <hr>
+  <b>Filmid:</b>
+  <?php echo readfilms(); ?>
+  <hr>
+</body>
+</html>
