dcb0b699d10821539f91ab17ac07f8f8542fa551
Harli Kodasma <harlikodasma@users.noreply.github.com>
2020-11-03 11:29:47 +0200

    üheksas tund lõpetatud

---
+++ tund9/fnc_user.php
@@ -0,0 +1,164 @@
+<?php
+	$database = "if20_harli_kod_vp_1";
+	
+	function signup($firstname, $lastname, $email, $gender, $birthdate, $password) {
+		$notice = null;
+		$conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+		$conn->set_charset("utf8");
+		$stmt = $conn->prepare("INSERT INTO vpusers (firstname, lastname, birthdate, gender, email, password) VALUES (?, ?, ?, ?, ?, ?)");
+		echo $conn->error;
+		
+		//krüpteerime salasõna
+		$options = ["cost" => 12, "salt" => substr(sha1(rand()), 0, 22)];
+		$pwdhash = password_hash($password, PASSWORD_BCRYPT, $options);
+		
+		$stmt->bind_param("sssiss", $firstname, $lastname, $birthdate, $gender, $email, $pwdhash);
+		
+		if($stmt->execute()) {
+			$notice = "ok";
+		} else {
+			$notice = $stmt->error;
+		}
+		$stmt->close();
+		$conn->close();
+	
+		return $notice;
+	}
+	
+	function signin($email, $password) {
+		$notice = null;
+		$conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+		$conn->set_charset("utf8");
+		$stmt = $conn->prepare("SELECT password FROM vpusers WHERE email = ?");
+		echo $conn->error;
+		$stmt->bind_param("s", $email);
+		$stmt->bind_result($passwordfromdb);
+		
+		if($stmt->execute()) {
+			//kui tehniliselt korras
+			if($stmt->fetch()) {
+				//kasutaja leiti
+				if(password_verify($password, $passwordfromdb)) {
+					//parool õige
+					$stmt->close();
+					
+					//loen sisseloginud kasutaja infot
+					$stmt = $conn->prepare("SELECT vpusers_id, firstname, lastname FROM vpusers WHERE email = ?");
+					echo $conn->error;
+					$stmt->bind_param("s", $email);
+					$stmt->bind_result($idfromdb, $firstnamefromdb, $lastnamefromdb);
+					$stmt->execute();
+					$stmt->fetch();
+					//salvestame sessiooni muutujad
+					$_SESSION["userid"] = $idfromdb;
+					$_SESSION["userfirstname"] = $firstnamefromdb;
+					$_SESSION["userlastname"] = $lastnamefromdb;
+					
+					$stmt->close();
+					$stmt = $conn->prepare("SELECT bgcolor, txtcolor FROM vpuserprofiles WHERE userid = ?");
+					echo $conn->error;
+					$stmt->bind_param("i", $_SESSION["userid"]);
+					$stmt->bind_result($userbgcolor, $usertxtcolor);
+					if($stmt->execute()) {
+						if($stmt->fetch()) {
+							$_SESSION["userbgcolor"] = $userbgcolor;
+							$_SESSION["usertxtcolor"] = $usertxtcolor;
+						} else {
+							$_SESSION["userbgcolor"] = "#b0e0e6";
+							$_SESSION["usertxtcolor"] = "#000000";
+						}
+					}
+					
+					$stmt->close();
+					$conn->close();
+					header("Location: home.php");
+					exit();
+				} else {
+					$notice = "Vale salasõna!";
+				}
+			} else {
+				$notice = "Kasutajat " .$email  ." ei leitud!";
+			}
+		} else {
+			//tehniline viga
+			$notice = $stmt->error;
+		}
+		$stmt->close();
+		$conn->close();
+		return $notice;
+	}
+	
+	function storeuserprofile($description, $bgcolor, $txtcolor) {
+		//SQL
+		//kontrollime, kas profiil on olemas
+		//SELECT vpuserprofiles_id FROM vpuserprofiles WHERE userid = ?
+		//küsimärk asendada väärtusega
+		//$_SESSION["userid"]
+		
+		//kui profiili pole olemas, siis loome
+		//INSERT INTO vpuserprofiles (userid, description, bgcolor, txtcolor) VALUES (?, ?, ?, ?)
+		
+		//kui profiil on olemas, siis uuendame
+		//UPDATE vpuserprofiles SET description = ?, bgcolor = ?, txtcolor = ? WHERE userid = ?
+		
+		//execute jms võib loomisel/uuendamisel ühine olla
+		
+		$notice = null;
+		$conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+		$conn->set_charset("utf8");
+		$stmt = $conn->prepare("SELECT vpuserprofiles_id FROM vpuserprofiles WHERE userid = ?");
+		echo $conn->error;
+		$stmt->bind_param("i", $_SESSION["userid"]);
+		$stmt->bind_result($profileidfromdb);
+		if($stmt->execute()) {
+			if($stmt->fetch()) {
+				$stmt->close();
+				$stmt = $conn->prepare("UPDATE vpuserprofiles SET description = ?, bgcolor = ?, txtcolor = ? WHERE userid = ?");
+				echo $conn->error;
+				$stmt->bind_param("sssi", $description, $bgcolor, $txtcolor, $_SESSION["userid"]);
+				if($stmt->execute()) {
+					$notice = "Profiil uuendatud!";
+				} else {
+					$notice = $stmt->error;
+				}
+			} else {
+				$stmt->close();
+				$stmt = $conn->prepare("INSERT INTO vpuserprofiles (userid, description, bgcolor, txtcolor) VALUES (?, ?, ?, ?)");
+				echo $conn->error;
+				$stmt->bind_param("isss", $_SESSION["userid"], $description, $bgcolor, $txtcolor);
+				if($stmt->execute()) {
+					$notice = "Profiil salvestatud!";
+				} else {
+					$notice = $stmt->error;
+				}
+			}
+		} else {
+			$notice = $stmt->error;
+		}
+		$stmt->close();
+		$conn->close();
+		return $notice;
+	}
+	
+	function readuserdescription() {
+		//kui profiil on olemas, loeb kasutaja lühitutvustuse
+		
+		$notice = null;
+		$conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+		$conn->set_charset("utf8");
+		$stmt = $conn->prepare("SELECT description FROM vpuserprofiles WHERE userid = ?");
+		echo $conn->error;
+		$stmt->bind_param("i", $_SESSION["userid"]);
+		$stmt->bind_result($descriptionfromdb);
+		if($stmt->execute()) {
+			if($stmt->fetch()) {
+				$_SESSION["userdescription"] = $descriptionfromdb;
+			}
+		} else {
+			$notice = $stmt->error;
+		}
+		$stmt->close();
+		$conn->close();
+		return $notice;
+	}
+?>
---
+++ tund9/page.php
@@ -0,0 +1,176 @@
+<?php
+	session_start();
+	//var_dump($_POST); post input väärtused, premade array
+	require("../../../config.php");
+	require("fnc_user.php");
+	require("fnc_common.php");
+	
+	$emailerror = null;
+	$passworderror = null;
+	$loginstatus = null;
+	$email = null;
+	
+	$fulltimenow = date("d.m.Y H:i:s");
+	$hournow = date("H");
+	$partofday = "lihtsalt aeg";
+	$weekdaynameset = ["esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev", "pühapäev"];
+	$monthnameset = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+	$daynow = date("d");
+	$monthnumbernow = date("n");
+	$yearnow = date("Y");
+	$fullclocknow = date("H:i:s");
+	$weekdaynow = date("N");
+	
+	if($hournow < 6) {
+		$partofday = "uneaeg";
+	}
+	if($hournow > 5 and $hournow < 8) {
+		$partofday = "kooli minemise aeg";
+	}
+	if($hournow >= 8 and $hournow <= 18) {
+		$partofday = "õppimise aeg";
+	}
+	if($hournow > 18 and $hournow < 20) {
+		$partofday = "söömise aeg";
+	}
+	if($hournow > 19 and $hournow < 24) {
+		$partofday = "teleka aeg";
+	}
+	
+	//vaatame semestri kulgemist
+	$semesterstart = new DateTime("2020-8-31");
+	$semesterend = new DateTime("2020-12-13");
+	$semesterduration = $semesterstart->diff($semesterend);
+	$semesterdurationdays = $semesterduration->format("%r%a");
+	$today = new DateTime("now");
+	$todayformatted = $today->format("Y-n-j");
+	$semesterstartformatted = $semesterstart->format("Y-n-j");
+	$fromsemesterstartuntiltoday = $semesterstart->diff($today);
+	$daysfromsemesterstart = $fromsemesterstartuntiltoday->format("%r%a");
+	$daysuntilend = $semesterdurationdays - $daysfromsemesterstart;
+	$percentagecalculation = round(($daysfromsemesterstart * 100) / $semesterdurationdays, 2);
+	if($todayformatted == $semesterstartformatted) {
+		$semesterpercentage = "Semestri õppetööst on tehtud 0%.";
+		$semesterprintout = "Täna algab semester.";
+	} elseif($daysfromsemesterstart > 0 and $daysfromsemesterstart < $semesterdurationdays) {
+		$isitone = $semesterdurationdays - $daysfromsemesterstart;
+		$semesterpercentage = "Semestri õppetööst on tehtud " .$percentagecalculation ."%";
+		if($isitone == 1) {
+			$semesterprintout = "Semester käib. See on käinud juba " .$daysfromsemesterstart ." päeva." ." Lõpuni on jäänud " .$daysuntilend ." päev.";
+		} elseif($daysfromsemesterstart == 1) {
+			$semesterprintout = "Semester käib. See on käinud juba " .$daysfromsemesterstart ." päev." ." Lõpuni on jäänud " .$daysuntilend ." päeva.";
+		} elseif($isitone > 1 and $daysfromsemesterstart > 1) {
+			$semesterprintout = "Semester käib. See on käinud juba " .$daysfromsemesterstart ." päeva." ." Lõpuni on jäänud " .$daysuntilend ." päeva.";
+		}
+	} elseif($daysfromsemesterstart == $semesterdurationdays) {
+		$semesterprintout = "Täna on semestri viimane päev. Semester on käinud " .$semesterdurationdays ." päeva.";
+		$semesterpercentage = "Semestri õppetööst on tehtud " .$percentagecalculation ."%";
+	} elseif($daysfromsemesterstart > $semesterdurationdays) {
+		$daysfromsemesterend = $daysfromsemesterstart - $semesterdurationdays;
+		$semesterpercentage = "Semestri õppetööst on tehtud 100%.";
+		if($daysfromsemesterend == 1) {
+			$semesterprintout = "Semester on läbi. Semestri lõpust on möödas " .$daysfromsemesterend ." päev.";
+		} else {
+		$semesterprintout = "Semester on läbi. Semestri lõpust on möödas " .$daysfromsemesterend ." päeva.";
+		}
+	} elseif($daysfromsemesterstart == -0) {
+		$fromsemesterstartuntiltoday = $today->diff($semesterstart);
+		$daysfromsemesterstart += 1;
+		$semesterpercentage = "Semestri õppetööst on tehtud 0%.";
+		$semesterprintout = "Semester ei ole veel alanud. Alguseni on jäänud " .$daysfromsemesterstart ." päev.";
+	} elseif($daysfromsemesterstart < 0) {
+		$daysfromsemesterstart = -$daysfromsemesterstart + 1;
+		$semesterpercentage = "Semestri õppetööst on tehtud 0%.";
+		$semesterprintout = "Semester ei ole veel alanud. Alguseni on jäänud " .$daysfromsemesterstart ." päeva.";
+	}
+	//2020-8-31
+	//2020-12-13
+	
+	//annan ette lubatud pildivormingute loendi
+	$picfiletypes = ["image/jpeg", "image/png"];
+	//loeme piltide kataloogi sisu ja näitame pilte
+	$allfiles = array_slice(scandir("../vp_pics/"), 2); //slice sest 2 esimest pole õiged failid
+	//$picfiles = array_slice($allfiles, 2);
+	$picfiles = [];
+	foreach($allfiles as $thing) {
+		$fileinfo = getImagesize("../vp_pics/" .$thing);
+		if(in_array($fileinfo["mime"], $picfiletypes)) {
+			array_push($picfiles, $thing);
+		}
+	}
+	
+	//paneme kõik pildid ekraanile
+	$piccount = count($picfiles);
+	//$i = $i + 1;
+	//$i ++;
+	//$i += 2;
+	//enne panime siin kõik pildid nähtavale, nüüd valime ühe suvalise
+	//$imghtml = "";
+	//for($i = 0; $i < $piccount; $i ++) {
+		//$imghtml .= '<img src="../vp_pics/' .$picfiles[$i] .'" alt="Tallinna Ülikool">'; //.= append
+	//}
+	$whichpic = mt_rand(0, ($piccount - 1));
+	$imghtml = '<img src="../vp_pics/' .$picfiles[$whichpic] .'" alt="Tallinna Ülikool">';
+	
+	//login
+	if(!empty($_POST["loginsubmit"])) {
+		if(empty($_POST["emailinput"])) {
+			$emailerror = "E-post sisestamata!";
+		} else {
+			if((filter_var(test_input($_POST["emailinput"]), FILTER_VALIDATE_EMAIL)) == null) {
+				$emailerror = "Ebakorrektne e-posti aadress!";
+			} else {
+				$email = test_input($_POST["emailinput"]);
+			}
+		}
+		if(strlen($_POST["passwordinput"]) < 8) {
+			if(empty($_POST["passwordinput"])) {
+				$passworderror = "Salasõna sisestamata!";
+			} else {
+				$passworderror = "Salasõna peab olema vähemalt 8 tähemärki pikk!";
+			}
+		}
+		if(empty($emailerror) and empty($passworderror)) {
+			$loginstatus = signin($email, $_POST["passwordinput"]);
+		}
+	}
+	
+	require("header.php");
+?>
+
+  <audio src="../sound/templeos.mp3" autoplay="autoplay" loop="loop"></audio>
+  
+  <img src="../img/vp_banner_improved.png" alt="Veebiprogrammeerimise kursuse bänner">
+  <h1>Noic veebileht</h1>
+  <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p>See leht on tehtud veebiprogrammeerimise kursusel 2020. aasta sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
+  <hr>
+  
+  <ul>
+	<li><a href="createaccount.php">Uue kasutaja loomine</a></li>
+  </ul>
+  
+  <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+	  <label for="emailinput">Kasutajatunnus (e-posti aadress)</label>
+	  <input type="email" name="emailinput" id="emailinput" placeholder="rick.tald@yandex.ru" value="<?php echo $email; ?>">
+	  <span><?php echo $emailerror; ?></span>
+	  <br>
+	  <label for="passwordinput">Salasõna</label>
+	  <input type="password" name="passwordinput" id="passwordinput" placeholder="😂😂😂😂😂">
+	  <span><?php echo $passworderror; ?></span>
+	  <br>
+	  <input type="submit" name="loginsubmit" value="Logi sisse">
+    </form>
+  <b><?php echo $loginstatus; ?></b>
+  
+  <p>Lehe avamise hetk: <?php echo $weekdaynameset[$weekdaynow - 1] .", " .$daynow .". " .$monthnameset[$monthnumbernow - 1] ." " .$yearnow .", kell " .$fullclocknow; ?>.</p>
+  <p><?php echo "Praegu on " .$partofday ."."; ?></p>
+  
+  <p>Semester kestab kokku <?php echo $semesterdurationdays; ?> päeva.</p>
+  <p><?php echo $semesterprintout; ?></p>
+  <p><?php echo $semesterpercentage; ?></p>
+  <hr>
+  <?php echo $imghtml; ?>
+  <hr>
+</body>
+</html>
